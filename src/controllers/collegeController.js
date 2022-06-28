f9311a10be1a885f178f758d4ac7836cc8a4849c
unknown <nagpureanshul7@gmail.com>
2022-06-28 19:55:37 +0530

    getcollege

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -1,13 +1,30 @@
 const collegeModel = require("../models/collegeModel");
+const internModel = require("../models/internModel");
 
 const createCollege = async function (req, res) {
   try {
     let data = req.body;
     let college = await collegeModel.create(data);
-    return res.status(201).send({ status: true, data: { college } });
+    return res.status(201).send({ status: true, data:  college  });
   } catch (err) {
     return res.status(500).send({ status: false, message: err.message });
   }
 };
 
 module.exports.createCollege = createCollege;
+
+
+const getCollegeDetails = async function (req, res){
+ 
+  const collegeName = req.query.collegeName
+   
+  const getCollege = await collegeModel.findOne({name: collegeName})
+  
+   const getCollegeId= getCollege._id
+   const internData = await internModel.find({collegeId: getCollegeId})
+   const data = {...getCollege.toJSON(), interns: internData}
+  
+   return res.status(200).send({ status: true, data: data});
+}
+
+module.exports.getCollegeDetails = getCollegeDetails;
--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -9,10 +9,10 @@ const createIntern = async function (req, res) {
     let collegeId = await collegeModel.findOne({ $or : [{name : collegeName}, {fullName : collegeName}]}).select({_id :1})
 
     delete data.collegeName
-    data.collegeId = collegeId._id.toString()
+    data.collegeId = collegeId._id
 
     let intern = await internModel.create(data);
-    return res.status(201).send({ status: true, data: {intern} });
+    return res.status(201).send({ status: true, data: intern });
   } catch (err) {
     return res.status(500).send({ status: false, message: err.message });
   }
