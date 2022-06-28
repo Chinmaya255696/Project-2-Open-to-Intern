c08ab0ca94f0b6760de9716e490371b2fdd20f9d
chinmaya255696 <chinmayasahoo.v@gmail.com>
2022-06-28 04:33:45 -0700

    models -updated

---
+++ src/models/collegeModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose")
+
+const collegeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        unique: true,
+        required: true,
+        trim:true
+    },
+    fullName: { 
+        type: String,
+        required: true,
+        trim:true
+    },
+    logoLink: { 
+        type:String,
+        required: true,
+        trim:true
+    },
+    isDeleted: { 
+        type:Boolean, 
+        default: false
+     }
+},{timestamps:true})    
+
+module.exports=mongoose.model("College",collegeSchema)
+   
+
+
+
---
+++ src/models/internModel.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose")
+
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const internSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    mobile: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    collegeId: {
+        type: ObjectId,
+        ref: "College",
+        required: true,
+        trim: true
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    }
+}, { timestamps: true })
+
+module.exports = mongoose.model("Intern", internSchema)
+
+
+
+
+
+
+
+
+
+
