4198fc85b34324172c5f45dc331e3f446b16b3d7
Ankita220296 <sanganiankita223@gmail.com>
2022-06-29 12:08:28 +0530

    Validations

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -1,30 +1,93 @@
 const collegeModel = require("../models/collegeModel");
 const internModel = require("../models/internModel");
 
+const isValid = function (value) {
+  if (Object.keys(value).length === 0) return false;
+  else return true;
+};
+
+const isValidValue = function (value) {
+  if (typeof value !== "string") return false;
+  else if (value.trim().length == 0) return false;
+  else return true;
+};
+
 const createCollege = async function (req, res) {
   try {
     let data = req.body;
+    if (!isValid(data))
+      return res
+        .status(400)
+        .send({ status: false, message: "No information pass" });
+    const { name, fullName, logoLink } = req.body;
+
+    if (!name)
+      return res
+        .status(400)
+        .send({ status: false, message: "Name is required" });
+    if (!isValidValue(name))
+      return res
+        .status(400)
+        .send({ status: false, message: "Name is in wrong format" });
+
+    if (!fullName)
+      return res
+        .status(400)
+        .send({ status: false, message: "Full Name is required" });
+    if (!isValidValue(fullName))
+      return res
+        .status(400)
+        .send({ status: false, message: "Full Name is in wrong format" });
+
+    if (!logoLink)
+      return res
+        .status(400)
+        .send({ status: false, message: "Logo link is required" });
+    if (!isValidValue(logoLink))
+      return res
+        .status(400)
+        .send({ status: false, message: "Logo link is in wrong format" });
+
+    try {
+      url = new URL(logoLink);
+    } catch (err) {
+      return res
+        .status(400)
+        .send({ status: false, message: "Logo link is invalid" });
+    }
+    if (!logoLink.match(/\.(jpeg|jpg|gif|png)$/))
+      return res
+        .status(400)
+        .send({ status: false, message: "Logo link is not showing image" });
+
+    let collegeName = await collegeModel.findOne({ name: req.body.name });
+    if (collegeName)
+      return res
+        .status(400)
+        .send({ status: false, message: "College Name is already exist" });
+
     let college = await collegeModel.create(data);
-    return res.status(201).send({ status: true, data:  college  });
+    return res.status(201).send({ status: true, data: college });
   } catch (err) {
     return res.status(500).send({ status: false, message: err.message });
   }
 };
 
-module.exports.createCollege = createCollege;
+// If there is no data in query params then return all interns
+// If collegeName is not found then show error
+const getCollegeDetails = async function (req, res) {
+  const collegeName = req.query.collegeName;
 
+  const college = await collegeModel.findOne({ name: collegeName });
 
-const getCollegeDetails = async function (req, res){
- 
-  const collegeName = req.query.collegeName
-   
-  const getCollege = await collegeModel.findOne({name: collegeName})
-  
-   const getCollegeId= getCollege._id
-   const internData = await internModel.find({collegeId: getCollegeId})
-   const data = {...getCollege.toJSON(), interns: internData}
-  
-   return res.status(200).send({ status: true, data: data});
-}
+  const getCollegeId = college._id;
+  const internData = await internModel.find({ collegeId: getCollegeId });
+  const data = { ...college.toJSON(), interns: internData };
 
-module.exports.getCollegeDetails = getCollegeDetails;
+  return res.status(200).send({ status: true, data: data });
+};
+
+module.exports = {
+  createCollege,
+  getCollegeDetails,
+};
--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -1,15 +1,29 @@
 const internModel = require("../models/internModel");
 const collegeModel = require("../models/collegeModel");
 
+// If there is no data in req.body
+// If name is not given 
+//If name has empty string or number or undefined or null
+// If email is not given
+// If email is not valid by regex
+// If email id is already given in database
+// If mobile number is not given
+// If mobile number is already exist in database
+// If mobile number is not unique check through regex
+// If college id is not valid check through isValid method
 const createIntern = async function (req, res) {
   try {
     let data = req.body;
-    let collegeName = data.collegeName
+    let collegeName = data.collegeName;
 
-    let collegeId = await collegeModel.findOne({ $or : [{name : collegeName}, {fullName : collegeName}]}).select({_id :1})
+    // TODO : change variable name. It should be college
+    let college = await collegeModel
+      .findOne({ $or: [{ name: collegeName }, { fullName: collegeName }] })
+      .select({ _id: 1 });
+   
 
-    delete data.collegeName
-    data.collegeId = collegeId._id
+    delete data.collegeName;
+    data.collegeId = college._id;
 
     let intern = await internModel.create(data);
     return res.status(201).send({ status: true, data: intern });
